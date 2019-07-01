9e1e387fed5bdbb842c516af413bd51bee0df3e7
itsStrobe <josezavala0297@gmail.com>
2019-07-01 12:24:44 -0500

    Scripts for identification of potential VNICs and extraction of instances of said VNCs in the corpora.

---
+++ src/FindVNICs.sh
@@ -0,0 +1,9 @@
+
+# Extract the Different Patterns Inside the Processed Corpus
+
+python3 ExtractPatternsInCorpora.py
+
+# Extract the Potential VNICs based on PMI
+
+python3 ExtractPotentialVNICs.py
+python3 ExtractVNICsInstances.py
