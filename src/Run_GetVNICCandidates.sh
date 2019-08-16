c6e903ddf632eae54bce536b4e504fb001862608
itsStrobe <josezavala0297@gmail.com>
2019-08-15 22:37:45 -0500

    Bug fix.

--- src/Run_GetVNICCandidates.sh
+++ src/Run_GetVNICCandidates.sh
@@ -10,8 +10,16 @@ source /usr/local/gpuallocation.sh
 
 echo "Run_GetVNICCandidates.sh"
 
+# - VIRTUAL EVIRONMENT SETUP - #
+virtualenv -p python36 venv && source venv/bin/activate
+pip3 install -r requirements.txt
+python36 -c "import nltk; nltk.download('wordnet')"
+################################
+
 python36 ExtractPotentialVNICs.py --FREQ_T 0 \
                                   --VNIC_DIR_PMI "./VNICs/PotentialVNICs_PMI_NoMin.csv" \
                                   --VNIC_DIR_LEX "./VNICs/PotentialVNICs_LEX_NoMin.csv" \
                                   --VNIC_DIR_SYN "./VNICs/PotentialVNICs_SYN_NoMin.csv" \
                                   --VNIC_DIR_OVA "./VNICs/PotentialVNICs_OVA_NoMin.csv"
+
+deactivate
